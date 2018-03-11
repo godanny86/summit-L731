3c5fd66140898ef70704919b66f9bebe88524012
Daniel Gordon <dgordon@adobe.com>
2018-03-11 15:18:58 +0000

    initial pass of summit content.

---
+++ summit-L731-aem/ui.apps/src/main/content/jcr_root/apps/summit-l731/clientlibs/clientlib-site/components/contentfragment/contentfragment.js
@@ -0,0 +1,48 @@
+//Wrap bindings in anonymous namespace to prevent collisions
+jQuery(function($) {
+	"use strict";
+	var PROCESSED_CF = "data-cf-processed", 
+		IMAGE_ELEMENT = ".cmp-contentfragment__element--speakerImage",
+		DOB_ELEMENT = ".cmp-contentfragment__element--speakerDOB",
+		CF_ELEMENT_VALUE = ".cmp-contentfragment__element-value",
+		THUMBNAIL_RENDITION = "/_jcr_content/renditions/cq5dam.thumbnail.319.319.png?ch_ck=1520390068000";
+
+	function applyComponentStyles() {
+
+		//Top Level Navigation (expected to only be one of these)
+		$(".cmp-cf-speaker").not("[" + PROCESSED_CF + "='true']").each(
+				function() {
+					// Mark the component element as processed to avoid the cyclic processing (see .not(..) above).
+					var cf = $(this).attr(PROCESSED_CF, true), 
+						imageEl = $(cf).find(IMAGE_ELEMENT),
+						imageValue, 
+						imageSrc,
+						dobEl = $(cf).find(DOB_ELEMENT),
+						dobValue;
+						
+
+					if (imageEl !== undefined) {
+						imageValue = $(imageEl).find(CF_ELEMENT_VALUE);
+						imageSrc = $(imageValue).text().trim();
+
+						if (imageSrc !== undefined) {
+							$(this).prepend(
+									"<div class='cmp-speaker-img'><img alt='speaker photo'" + "src='"
+											+ imageSrc + THUMBNAIL_RENDITION
+											+ "' /> <div class='border-box-highlight' /></div>");
+						}
+					}
+					
+					if(dobEl !== undefined) {
+						dobValue = $(dobEl).find(CF_ELEMENT_VALUE);
+						if(dobValue !== undefined) {
+							dobValue.text(dobValue.text().trim().substring(0, 10));
+						}
+					}
+				});
+	}
+
+	applyComponentStyles();
+
+	$(".responsivegrid").bind("DOMNodeInserted", applyComponentStyles);
+});
