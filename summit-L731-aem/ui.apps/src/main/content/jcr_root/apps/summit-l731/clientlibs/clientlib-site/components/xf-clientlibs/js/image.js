319d6e479bbc4e48a42bed4b98d4c5bd7c3a7506
Daniel Gordon <dgordon@adobe.com>
2018-03-17 17:21:16 +0000

    updated for XF demo.

---
+++ summit-L731-aem/ui.apps/src/main/content/jcr_root/apps/summit-l731/clientlibs/clientlib-site/components/xf-clientlibs/js/image.js
@@ -0,0 +1,33 @@
+// Wrap bindings in anonymous namespace to prevent collisions
+jQuery(function($) {
+    "use strict";
+    var CMP_SELECTOR = ".cmp-image--border",
+        DATA_PROCESSED = "data-image-processed",
+        IMG_SELECTOR = ".cmp-image__image",
+    		IMG_CONTAINER = ".cmp-image";
+        
+
+ function applyComponentStyles() {
+
+  //Top Level Navigation (expected to only be one of these)
+  $(CMP_SELECTOR).not("[" + DATA_PROCESSED + "='true']").each(function() {
+            // Mark the component element as processed to avoid the cyclic processing (see .not(..) above).
+            var component = $(this).attr(DATA_PROCESSED, true),
+            image = $(this).find(IMG_SELECTOR),
+            imgContainer = $(this).find(IMG_CONTAINER),
+            title;
+            console.log("here?");
+            if(image !== undefined) {
+            	    title = $(image).attr("title");
+            	    console.log("title!" + title);
+            	    imgContainer.append("<div class='border-box-highlight'></div>");
+            	    component.append("<div class='speakerTitle'>" + title + "</div>");
+            }
+          
+          });
+    }
+
+  applyComponentStyles();
+  
+  $(".responsivegrid").bind("DOMNodeInserted", applyComponentStyles);
+});
